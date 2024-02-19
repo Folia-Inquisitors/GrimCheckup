776fe2e8213843bf8b7dc7eb5352f76c34719f7e
HSGamer <huynhqtienvtag@gmail.com>
2024-02-19 14:52:51 +0700

    init

---
+++ src/main/java/me/hsgamer/grimcheckup/GrimCheckup.java
@@ -0,0 +1,56 @@
+package me.hsgamer.grimcheckup;
+
+import ac.grim.grimac.AbstractCheck;
+import ac.grim.grimac.events.FlagEvent;
+import ac.grim.grimac.manager.CheckManager;
+import ac.grim.grimac.player.GrimPlayer;
+import ac.grim.grimac.shaded.com.github.retrooper.packetevents.protocol.player.User;
+import ac.grim.grimac.shaded.com.github.retrooper.packetevents.protocol.player.UserProfile;
+import com.google.common.collect.ClassToInstanceMap;
+import me.hsgamer.hscore.bukkit.config.BukkitConfig;
+import me.hsgamer.hscore.bukkit.simpleplugin.SimplePlugin;
+import me.hsgamer.hscore.config.PathString;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+import java.util.*;
+
+public final class GrimCheckup extends SimplePlugin implements Listener {
+    private final BukkitConfig config = new BukkitConfig(this);
+    private final Set<String> disabledChecks = new HashSet<>();
+
+    @Override
+    public void enable() {
+        config.setup();
+
+        GrimPlayer grimPlayer = fakeGrimPlayer();
+        CheckManager checkManager = new CheckManager(grimPlayer);
+        ClassToInstanceMap<AbstractCheck> checks = checkManager.allChecks;
+
+        checks.forEach((clazz, check) -> {
+            String configName = check.getConfigName();
+            PathString pathString = new PathString(configName);
+            config.setIfAbsent(pathString, true);
+
+            Object value = config.getNormalized(pathString);
+            if (value instanceof Boolean && !(Boolean) value) {
+                disabledChecks.add(configName);
+            }
+        });
+
+        registerListener(this);
+    }
+
+    @EventHandler
+    public void onFlag(FlagEvent event) {
+        AbstractCheck check = event.getCheck();
+        if (disabledChecks.contains(check.getConfigName())) {
+            event.setCancelled(true);
+        }
+    }
+
+    private GrimPlayer fakeGrimPlayer() {
+        User user = new User(null, null, null, new UserProfile(UUID.randomUUID(), "Fake"));
+        return new GrimPlayer(user);
+    }
+}
